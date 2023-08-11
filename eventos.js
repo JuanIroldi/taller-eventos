528e5f1728f84321326d1aa810337ccbc97fc2ff
JuanIroldi <juaniroldi2003@gmail.com>
2023-08-10 23:48:30 -0300

    Solucion evento-click-div

--- eventos.js
+++ eventos.js
@@ -1,12 +1,12 @@
-const saludarButton = document.getElementById('saludarButton');
-
-saludarButton.onclick = function() {
-    alert('Hola!');
-};
-
 const divSaludo = document.getElementById('divSaludo');
 
 divSaludo.addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
 
+const saludarButton = document.getElementById('saludarButton');
+
+saludarButton.addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation(); // Detiene la propagación del evento al div
+});
